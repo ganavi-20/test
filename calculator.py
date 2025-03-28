0abde251e9a206d17740be260af855d23bc11e79
ganavi-20 <ganavireddy0803@gmail.com>
2025-03-28 12:10:21 +0530

    first code

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mult(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+print("select operations.")
+print("add")
+print("sub")
+print("mult")
+print("div")
+while true    choice = input("enter choice(1/2/3/4
