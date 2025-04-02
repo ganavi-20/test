5df0991757ec8daf59dbbf4c7b1eb1605e284271
ganavi-20 <ganavireddy0803@gmail.com>
2025-04-02 10:41:49 +0530

    tic tac toe commit 2

--- ttt.py
+++ ttt.py
@@ -2,4 +2,16 @@ def print_board(board):
     for row in board:
         print(" | ".join(row))
         print("-" * 5)
+ def check_winner(board):
+    
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != " ":
+            return True
+        if board[0][i] == board[1][i] == board[2][i] != " ":
+            return True
+    if board[0][0] == board[1][1] == board[2][2] != " ":
+        return True
+    if board[0][2] == board[1][1] == board[2][0] != " ":
+        return True
+    return False
 
