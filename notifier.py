1df92712193a0e2b888d83d04515f8016c3a1bee
ganavi-20 <ganavireddy0803@gmail.com>
2025-04-16 11:07:27 +0530

    desktop notifier using python

--- notifier.py
+++ notifier.py
@@ -1,3 +1,7 @@
 from plyer import notification
 import time
-
+def send_notification(title, message, timeout=10):
+    notification.notify(
+        title=title,
+        message=message,
+        timeout=timeout)
