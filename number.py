fd8f97c90a7a5c938f7935d447eb2ce3813042b0
ganavi-20 <ganavireddy0803@gmail.com>
2025-04-16 10:25:02 +0530

    number guessing game using python

--- number.py
+++ number.py
@@ -4,3 +4,16 @@ secret_number = random.randint(1, 100)
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+
+        if guess < secret_number:
+            print("Too low. Try again!")
+        elif guess > secret_number:
+            print("Too high. Try again!")
+        else:
+            print("Congratulations! You guessed it!")
+            break
+    except ValueError:
+        print("Please enter a valid number.")
