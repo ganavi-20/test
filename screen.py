3492036ec3b32dc727ed2755388385b9c5310b67
ganavi-20 <ganavireddy0803@gmail.com>
2025-04-16 11:03:17 +0530

    screen recorder using python

--- screen.py
+++ screen.py
@@ -10,4 +10,11 @@ out = cv2.VideoWriter('screen_recording.avi', fourcc, fps, screen_size)
 
 print("Screen recording started...")
 start_time = time.time()
+duration = 10
 
+while True:
+    
+    img = ImageGrab.grab(bbox=(0, 0, screen_size[0], screen_size[1]))
+    img_np = np.array(img)
+    
+    frame = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
