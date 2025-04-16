b92dacff04b7f47a99a69e08228f32937d660aef
ganavi-20 <ganavireddy0803@gmail.com>
2025-04-16 10:33:44 +0530

    2048 game using python

--- 2048.py
+++ 2048.py
@@ -27,3 +27,38 @@ def merge(row):
             new[i] *= 2
             new[i+1] = 0
     return [i for i in new if i != 0] + [0]*(4-len([i for i in new if i != 0]))
+def move_left(board):
+    return [merge(row) for row in board]
+
+def move_right(board):
+    return [merge(row[::-1])[::-1] for row in board]
+
+def move_up(board):
+    rotated = list(zip(*board))
+    moved = move_left([list(row) for row in rotated])
+    return [list(row) for row in zip(*moved)]
+
+def move_down(board):
+    rotated = list(zip(*board))
+    moved = move_right([list(row) for row in rotated])
+    return [list(row) for row in zip(*moved)]
+
+def can_move(board):
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] == 0:
+                return True
+            if j < 3 and board[i][j] == board[i][j+1]:
+                return True
+            if i < 3 and board[i][j] == board[i+1][j]:
+                return True
+    return False
+
+def play():
+    board = init_board()
+    while True:
+        print_board(board)
+        move = input("Use WASD to move (w=up, s=down, a=left, d=right): ").lower()
+
+        if move not in 'wasd':
+            continue
