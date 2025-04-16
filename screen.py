00d1877300f4e7bba2a80082143d1dec597baf15
ganavi-20 <ganavireddy0803@gmail.com>
2025-04-16 11:02:02 +0530

    screen recorder using python

--- screen.py
+++ screen.py
@@ -3,3 +3,11 @@ import cv2
 import pyautogui
 from PIL import ImageGrab
 import time
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fps = 20.0
+screen_size = pyautogui.size()
+out = cv2.VideoWriter('screen_recording.avi', fourcc, fps, screen_size)
+
+print("Screen recording started...")
+start_time = time.time()
+
